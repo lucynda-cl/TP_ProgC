ffcf6282dd4f65bf397346ca698a8ef75a8d6aad
lucynda-cl <chevreuil.lucynda@gmail.com>
2026-09-18 09:22:46 +0000

    Ajouter le calcul du cercle

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
-int main() {
-double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+int main(void)
+{
+    double pi = 3.14159;
+    double rayon = 6.0;
+
+    double aire = pi * rayon * rayon;
+    double perimetre = 2 * pi * rayon;
+
+    printf("Aire du cercle : %f\n", aire);
+    printf("Perimetre du cercle : %f\n", perimetre);
+
+    return 0;
 }
 
